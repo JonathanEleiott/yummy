0864f1d4b244e38f0a80eef0f0d5252868dd2bd0
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2025-02-20 12:10:27 -0500

    add getAllFoods and getFoodById functionality

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const { getAllFoods, getFoodById } = require('./db/foods.js');
+
+const client = require('./db/client.js');
+client.connect();
+
 const express = require('express');
 const app = express();
 
@@ -5,8 +10,33 @@ app.get('/', (req, res, next) => {
   res.send('WELCOME');
 });
 
+app.get('/api/v1/foods', async(req, res, next) => {
+  try {
+    const allFoods = await getAllFoods();
+    res.send(allFoods);
+  } catch(err) {
+    next(err);
+  }
+});
+
+app.get('/api/v1/foods/:id', async(req, res, next) => {
+  try {
+    const { id } = req.params;
+    const foundFood = await getFoodById(id);
+
+    if(!foundFood) {
+      next({ status: 400, message: 'Food Not Found'});
+    }
+  
+    res.send(foundFood);
+  } catch(err) {
+    next(err);
+  }
+});
+
 app.use((req, res, next) => {
-  res.status(404).send('Page Not Found');
+  next({ status: 404, message: 'Page Not Found'});
+  // res.status(404).send('Page Not Found');
 });
 
 app.use((err, req, res, next) => {
