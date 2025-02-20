06c1075789a26a629151b30a2f64f59c0719f6fb
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2025-02-20 13:08:41 -0500

    add list to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from 'react';
+
 const App = () => {
+  const [allFoods, setAllFoods] = useState([]);
+
+  useEffect(() => {
+    const getAllFoods = async() => {
+      const response = await fetch('/api/v1/foods');
+      const retrievedFoods = await response.json();
+      setAllFoods(retrievedFoods);
+    }
+    
+    getAllFoods();
+  }, []);
   
   return (
     <>
       <h1>Yummy</h1>
+
+      <ol>
+      {
+        allFoods.map((singleFood) => {
+          return <li key={singleFood.id}>{singleFood.name}</li>
+        })
+      }
+      </ol>
     </>
   )
 }
